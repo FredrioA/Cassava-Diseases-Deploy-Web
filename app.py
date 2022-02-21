8391c418c7c5669f2bfec931713d09126900ac68
Fredrio Arcello <61153730+FredrioA@users.noreply.github.com>
2022-02-21 10:42:16 +0700

    update app

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def predict_label(img_path):
     img_array = img_to_array(loaded_img)
     img_array = expand_dims(img_array, axis=0)
     images = np.vstack([img_array])
-    classes = model.predict(images)
+    predicted_bit = model.predict(images)
     return class_dict[predicted_bit]
 
 @app.route('/', methods=['GET', 'POST'])
